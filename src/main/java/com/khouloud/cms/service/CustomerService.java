73a5fad031da9b0a1a13fd7273ca9e5b6c86e33d
khouloud711 <bejaouikhouloud98@gmail.com>
2021-02-25 00:48:15 +0100

    test

--- src/main/java/com/khouloud/cms/service/CustomerService.java
+++ src/main/java/com/khouloud/cms/service/CustomerService.java
@@ -20,7 +20,8 @@ public class CustomerService {
     private List<Customer> CustomerList = new CopyOnWriteArrayList<>();
 
 
-    
+
+
     //create customer
     public Customer addCustomer(Customer customer) {
 
